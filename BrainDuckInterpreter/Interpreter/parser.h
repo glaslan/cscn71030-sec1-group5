1fbfe04da2e836e8de75753d72012f3a4fee192f
glaslan <31631023+glaslan@users.noreply.github.com>
2024-03-09 09:44:11 -0500

    Update parser.h

--- BrainDuckInterpreter/Interpreter/parser.h
+++ BrainDuckInterpreter/Interpreter/parser.h
@@ -1,1 +1,21 @@
 #pragma once
+
+void parseArguments();
+void parseProgram();
+
+void parseShiftRight();
+void parseShiftLeft();
+
+void parseIncrement();
+void parseDecrement();
+
+void parseOutput();
+void parseInput();
+
+void parseJumpForward();
+void parseJumpBack();
+
+void parseDouble();
+void parseHalve();
+
+void parseRandom();
