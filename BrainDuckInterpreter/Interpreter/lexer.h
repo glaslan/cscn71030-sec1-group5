a37d4096c2b29f65e5aaac31d7f2bda0769a3e05
DESTINY LOUANGSOMBATH <dlouangsombath5708@conestogac.on.ca>
2024-03-09 09:47:25 -0500

    please work

---
+++ BrainDuckInterpreter/Interpreter/lexer.h
@@ -0,0 +1,36 @@
+#pragma once
+// Destiny - Lexer
+
+// types of tokens 
+typedef enum {
+	MOVE_LEFT,		// <
+	MOVE_RIGHT,		// >
+	INCREMENT,		// +
+	DECREMENT,		// -
+	INPUT,			// ,
+	OUTPUT,			// .
+	JUMP_PAST,		// ]
+	JUMP_BACK,		// [
+	END_TOKEN		// reached end of file 
+}tokenType;
+
+// actual token struct
+typedef struct token {
+	tokenType t;
+}TOKEN;
+
+// lexer thingy struct
+typedef struct lexer {
+	char* input;
+	int position;
+	char current;
+}LEXER;
+
+// initialize the lexer (i.e. set the starting point)
+void initLexer();
+
+// advance to the next char 
+void goToNext();
+
+// turn the char into a token 
+TOKEN getToken(LEXER* l);
